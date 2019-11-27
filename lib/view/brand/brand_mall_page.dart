26620fb5d295226e2b179e068ebc4824e586aa48
chuhanybb185 <770522860@qq.com>
2019-11-27 11:45:10 +0800

    1

--- lib/view/brand/brand_mall_page.dart
+++ lib/view/brand/brand_mall_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:innetsect/base/base.dart';
 import 'package:innetsect/data/exhibition/brand_mall_model.dart';
 import 'package:innetsect/view/brand/brand_mall_provide.dart';
+import 'package:innetsect/view/widget/list_widget_page.dart';
 import 'package:provide/provide.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
 
 class BrandMallPage extends PageProvideNode {
   final BrandMallPrvide _prvide = BrandMallPrvide();
@@ -27,8 +29,10 @@ class BrandMallContentPage extends StatefulWidget {
 class _BrandMallContentPageState extends State<BrandMallContentPage> {
    BrandMallPrvide _prvide;
    String brandName;
+   ///控制器
+  EasyRefreshController _controller;
     int pageNo = 1;
-   @override
+   @override    
    void initState() { 
      super.initState();
      _prvide??= widget._prvide;
@@ -55,6 +59,9 @@ class _BrandMallContentPageState extends State<BrandMallContentPage> {
       //   title: Text('品牌商城'),
       //   centerTitle: true,
       // ),
+      body: ListWidgetPage(
+
+      ),
     );
   }
 }
